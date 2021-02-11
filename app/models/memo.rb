03e34381bc85b94924c1268c55926cdb5c717cd7
kasai441 <kasai441441@gmail.com>
2021-02-11 15:12:30 +0900

    Memoクラス最適化

--- app/models/memo.rb
+++ app/models/memo.rb
@@ -2,35 +2,33 @@ class Memo
   SAVE_FILE = './memo.json'
   
   def self.all
-    read_file(SAVE_FILE)
+    read_file
   end
 
   def self.new(params)
-    file_hash = read_file(SAVE_FILE)
     id = Time.now.strftime('%Y%m%d%H%M%S%L')
-    write_file(SAVE_FILE, file_hash, id, params)
+    write_file(id, params)
   end
 
   def self.update(params)
-    file_hash = read_file(SAVE_FILE)
-    id = params[:id]
-    write_file(SAVE_FILE, file_hash, id, params)
+    write_file(params[:id], params)
   end
 
   private
  
-  def self.read_file(file)
+  def self.read_file
     file_hash = {}
-    if File.exist?(file)
-      File.open(file, 'r') do |f|
+    if File.exist?(SAVE_FILE)
+      File.open(SAVE_FILE, 'r') do |f|
         file_hash = JSON.load(f)
       end
     end
     file_hash ||= {}
   end
   
-  def self.write_file(file, file_hash, id, params)
-    File.open(file, 'w') do |f|
+  def self.write_file(id, params)
+    file_hash = read_file
+    File.open(SAVE_FILE, 'w') do |f|
       file_hash[id] = { title: params[:title], content: params[:content] }
       str = JSON.dump(file_hash, f)
     end
