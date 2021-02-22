2e7db7ca5d6add9c5c0f27747504505ae8184b71
kasai441 <kasai441441@gmail.com>
2021-02-22 11:47:56 +0900

    MemoクラスのCRUD関係メソッドをクラスメソッド化

--- Gemfile
+++ Gemfile
@@ -4,8 +4,6 @@ source "https://rubygems.org"
 
 git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
-# gem "rails"
-
 gem "sinatra", "~> 2.1"
 
 gem "webrick", "~> 1.7"
--- app/models/memo.rb
+++ app/models/memo.rb
@@ -12,55 +12,58 @@ class Memo
     @content = content
   end
 
-  def all
-    memos = []
-    read_file.each do |id, memo|
-      memos << Memo.new(id, memo['title'], memo['content'])
+  class << self
+    def all
+      memos = []
+      read_file.each do |id, memo|
+        memos << Memo.new(id, memo['title'], memo['content'])
+      end
+      memos
     end
-    memos
-  end
 
-  def find(id)
-    file_hash = read_file
-    Memo.new(id, file_hash[id]['title'], file_hash[id]['content']) if file_hash[id]
-  end
-
-  def add(params)
-    id = Time.now.strftime('%Y%m%d%H%M%S%L')
-    write_id_params(id, params)
-  end
-
-  def update(params)
-    write_id_params(params[:id], params)
-  end
-
-  def destroy(id)
-    file_hash = read_file
-    file_hash.delete(id)
-    write_file(file_hash)
-  end
-
-  private
-
-  def read_file
-    file_hash = {}
-    if File.exist?(SAVE_FILE)
-      File.open(SAVE_FILE, 'r') do |f|
-        file_hash = JSON.parse(f.read)
+    def find(id)
+      if (data = read_file[id])
+        Memo.new(id, data['title'], data['content'])
       end
     end
-    file_hash ||= {}
-  end
 
-  def write_id_params(id, params)
-    file_hash = read_file
-    file_hash[id] = { title: params[:title], content: params[:content] }
-    write_file(file_hash)
-  end
+    def add(params)
+      id = Time.now.strftime('%Y%m%d%H%M%S%L')
+      write_id_params(id, params)
+    end
 
-  def write_file(file_hash)
-    File.open(SAVE_FILE, 'w') do |f|
-      JSON.dump(file_hash, f)
+    def update(params)
+      write_id_params(params[:id], params)
+    end
+
+    def destroy(id)
+      file_hash = read_file
+      file_hash.delete(id)
+      write_file(file_hash)
+    end
+
+    private
+
+    def read_file
+      file_hash = {}
+      if File.exist?(SAVE_FILE)
+        File.open(SAVE_FILE, 'r') do |f|
+          file_hash = JSON.parse(f.read)
+        end
+      end
+      file_hash ||= {}
+    end
+
+    def write_id_params(id, params)
+      file_hash = read_file
+      file_hash[id] = { title: params[:title], content: params[:content] }
+      write_file(file_hash)
+    end
+
+    def write_file(file_hash)
+      File.open(SAVE_FILE, 'w') do |f|
+        JSON.dump(file_hash, f)
+      end
     end
   end
 end
