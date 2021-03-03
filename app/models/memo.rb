08092be3bdf6a7f361bde53eb7eb93c70fdef5fb
kasai441 <kasai441441@gmail.com>
2021-03-03 10:51:29 +0900

    SQL文用のクラスとSQL呼び出し用クラスを統合

--- app/models/memo.rb
+++ app/models/memo.rb
@@ -1,9 +1,13 @@
 # frozen_string_literal: true
 
-require_relative './db'
+require 'pg'
+require 'yaml'
 
 class Memo
   attr_accessor :id, :title, :content
+
+  FILE_NAME = 'database.yml'
+  SETTINGS_FILE = "#{File.dirname(__FILE__)}/#{FILE_NAME}"
 
   def initialize(id = 0, title = '', content = '')
     @id = id
@@ -13,26 +17,47 @@ class Memo
 
   class << self
     def all
-      SQL.new.select_all.map do |memo|
+      conn = connect_db
+      stmt = 'SELECT * FROM memos'
+      conn.exec(stmt).map do |memo|
         Memo.new(memo['id'], memo['title'], memo['content'])
       end
     end
 
     def find(id)
-      memo = SQL.new.select(id)[0]
+      conn = connect_db
+      stmt = 'SELECT * FROM memos WHERE id = $1'
+      conn.prepare('select', stmt)
+      memo = conn.exec_prepared('select', [id])[0]
       Memo.new(memo['id'], memo['title'], memo['content'])
     end
 
     def add(params)
-      SQL.new.insert(params)
+      conn = connect_db
+      stmt = 'INSERT INTO memos (title, content) VALUES($1, $2)'
+      conn.prepare('insert', stmt)
+      conn.exec_prepared('insert', [params[:title], params[:content]])
     end
 
     def update(params)
-      SQL.new.update(params[:id], params)
+      conn = connect_db
+      stmt = 'UPDATE memos SET title = $2, content = $3 WHERE id = $1'
+      conn.prepare('update', stmt)
+      conn.exec_prepared('update', [params[:id], params[:title], params[:content]])
     end
 
     def destroy(id)
-      SQL.new.delete(id)
+      conn = connect_db
+      stmt = 'DELETE FROM memos WHERE id = $1'
+      conn.prepare('delete', stmt)
+      conn.exec_prepared('delete', [id])
+    end
+
+    private
+
+    def connect_db
+      conn_settings = File.open(SETTINGS_FILE, 'r') { |f| YAML.safe_load(f) }
+      PG::Connection.new(conn_settings['default'])
     end
   end
 end
